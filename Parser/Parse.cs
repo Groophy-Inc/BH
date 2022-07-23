6f71fba7d1d3e2420d21935230455862ecb9a451
GroophyLifefor <Groophylifefor@gmail.com>
2022-07-23 15:37:44 +0300

    Json parser with Newtonsoft.Json

--- Parser/Parse.cs
+++ Parser/Parse.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System.IO;
+using BH.Structes;
 
 namespace BH.Parser
 {
@@ -11,11 +9,16 @@ namespace BH.Parser
         public static string srcPath { get; set; }
         public static string masterPagePath { get; set; }
         public static string[] Options { get; set; }
+
         public static void ParseMasterPage(string _srcPath, string _masterPagePath, string[] _Options)
         {
             srcPath = _srcPath;
             masterPagePath = _masterPagePath;
             Options = _Options;
+
+            GetElements.Get = JsonConvert.DeserializeObject<AllElements>(File.ReadAllText(masterPagePath)).ElementList;
+
+                        
         }
     }
 }
--- Structes/AllElements.cs
+++ Structes/AllElements.cs
@@ -1,5 +1,10 @@
 ﻿namespace BH.Structes
 {
+    public class GetElements
+    {
+        public static Element[] Get { get; set; }
+    }
+
     public class AllElements
     {
         public Element[] ElementList { get; set; }
