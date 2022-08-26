8a11f78bb0d60dafda24cab5748bb82de1857f0f
GroophyLifefor <Groophylifefor@gmail.com>
2022-08-26 19:05:49 +0300

    CommandBuilder Create

---
+++ Parser/Commands/CommandBuilder/Base.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BH.Parser.Commands
+{
+    public class CommandBuilder
+    {
+        public pbp_Command Registery { get; set; } = new pbp_Command();
+
+        public void Create(string Commandname,string[] registery, Func<pbp_Command, int> Parser)
+        {
+            pbp_Command command = new pbp_Command();
+            command.CommandName = Commandname;
+
+            foreach (var reg in registery)
+            {
+                Command c = new Command();
+                string clear = reg.TrimStart('[').TrimEnd(']');
+
+                bool isSigned_Values = (clear.IndexOf('|') == -1) ? false : true;
+
+                if (isSigned_Values) //Signed_Values
+                {
+                    c.Type = CommandParseType.signed_values;
+                    string[] values = clear.Split('|');
+                    c.Value = values;
+                }
+                else if (clear.StartsWith('$')) //unsigned_value
+                {
+                    c.Type = CommandParseType.unsigned_value;
+                }
+                else if (clear.StartsWith("\"\"")) //Content
+                {
+                    c.Type = CommandParseType.content;
+                }
+                else //Signed_Value
+                {
+                    c.Type = CommandParseType.signed_value;
+                    c.Value = clear;
+                }
+                
+                command.Commands.Add(c);
+            }
+
+            command.Delegate = Parser;
+            Registery = command;
+        }
+    }
+}
