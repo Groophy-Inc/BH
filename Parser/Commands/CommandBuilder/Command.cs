92d1df4b05df9bcb479bfd37f8ae873a33dac1ca
GroophyLifefor <Groophylifefor@gmail.com>
2022-08-26 19:03:25 +0300

    CommandBuilder signed_value CommandBuilder signed_values CommandBuilder unsigned_value CommandBuilder content

---
+++ Parser/Commands/CommandBuilder/Command.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BH.Parser.Commands
+{
+    public enum CommandParseType
+    {
+        signed_value,   //[ums]
+        signed_values,  //[CF|PS|CS]
+        unsigned_value, //[$]
+        content,        //[""]
+    }
+
+    public class pbp_Command
+    {
+        public string CommandName { get; set; } = "NaN";
+        public List<Command> Commands { get; set; } = new List<Command>();
+        public Func<pbp_Command,int> Delegate { get; set; }
+    }
+    
+    public class Command
+    {
+        public CommandParseType Type { get; set; }
+        public object Value { get; set; }
+        public int StatusValue_NotForUsers { get; set; } = 0;
+        public object Result { get; set; }
+    }
+}
