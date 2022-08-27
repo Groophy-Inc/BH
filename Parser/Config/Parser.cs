17645bc2ed1504c5dafcf3d809f192a036b45c78
GroophyLifefor <Groophylifefor@gmail.com>
2022-08-27 22:03:29 +0300

    Config parser changed

--- Parser/Config/Parser.cs
+++ Parser/Config/Parser.cs
@@ -1,67 +1,73 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
-using MyProg;
+using CSScripting;
 
 namespace BH.Parser.Config
 {
     public class Parser
     {
-        public static IniFile Config { get; set; }
-        
+        public static Dictionary<string, Dictionary<string, string>> config { get; private set; } =
+            new Dictionary<string, Dictionary<string, string>>();
+
         public static void Parse()
         {
-            var MyIni = new IniFile("color.cfg");
-            Config = MyIni;
-        }
-    }
-}
+            string[] lines = File.ReadAllLines("config.cfg");
+            string lastest_section = "";
 
-// Change this to match your program's normal namespace
-namespace MyProg
-{
-    public class IniFile   // revision 11
-    {
-        string Path;
-        string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (string.IsNullOrEmpty(lines[i])) continue;
 
-        [DllImport("kernel32", CharSet = CharSet.Unicode)]
-        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+                string line = lines[i];
 
-        [DllImport("kernel32", CharSet = CharSet.Unicode)]
-        static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+                if (line.StartsWith('[') && line.EndsWith(']'))
+                {
+                    string section = line.TrimStart('[').TrimEnd(']');
+                    try
+                    {
+                        var ishave = config[section];
+                    }
+                    catch
+                    {
+                        config.Add(section, new Dictionary<string, string>());
+                    }
 
-        public IniFile(string IniPath = null)
-        {
-            Path = new FileInfo(IniPath ?? EXE + ".ini").FullName;
-        }
+                    lastest_section = section;
+                }
+                else
+                {
+                    try
+                    {
+                        string key = line.Split('=')[0];
+                        string value =string.Join('=',  line.Split('=').Skip(1).ToArray());
 
-        public string Read(string Key, string Section = null)
-        {
-            var RetVal = new StringBuilder(255);
-            GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
-            return RetVal.ToString();
-        }
-
-        public void Write(string Key, string Value, string Section = null)
-        {
-            WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
-        }
-
-        public void DeleteKey(string Key, string Section = null)
-        {
-            Write(Key, null, Section ?? EXE);
+                        var x = config[lastest_section];
+                        x.Add(key, value);
+                    }
+                    catch { }
+                }
+            }
+            return;
         }
 
-        public void DeleteSection(string Section = null)
+        public class Config
         {
-            Write(null, null, Section ?? EXE);
-        }
-
-        public bool KeyExists(string Key, string Section = null)
-        {
-            return Read(Key, Section).Length > 0;
+            public static string Read(string key, string section)
+            {
+                try
+                {
+                    return config[section][key];
+                }
+                catch (Exception e)
+                {
+                    return "Null";
+                }
+            }
         }
     }
 }
